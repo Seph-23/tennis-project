3daf43a30df39f33eb3d90cc0933ce10a9ec4ea7
Seph-23 <seongmlyu95@gmail.com>
2022-10-15 13:20:04 +0900

    .

--- secondboard/src/main/java/myweb/secondboard/repository/CommentRepository.java
+++ secondboard/src/main/java/myweb/secondboard/repository/CommentRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryInterface {
 
     @Query("select c from Comment c where c.board.id = :boardId")
-    List<Comment> findComments(Long boardId);
+    List<Comment> findComments(@Param("boardId") Long boardId);
 }
