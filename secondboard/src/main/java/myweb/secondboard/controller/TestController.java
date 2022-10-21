116beae9b8943b7313ce2f24cbb381f620376c24
Achiiun <rlduwl0404@gmail.com>
2022-10-21 16:13:17 +0900

    test

--- secondboard/src/main/java/myweb/secondboard/controller/TestController.java
+++ secondboard/src/main/java/myweb/secondboard/controller/TestController.java
@@ -1,7 +1,9 @@
 package myweb.secondboard.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class TestController {
 
   @GetMapping("/test")
