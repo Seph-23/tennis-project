7dd2a618ef1257b4b2d0a63418e39372ded70ce8
euichanhwang <pine6971@naver.com>
2022-10-21 15:58:49 +0900

    test controller

---
+++ secondboard/src/main/java/myweb/secondboard/controller/TestController.java
@@ -0,0 +1,15 @@
+package myweb.secondboard.controller;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class TestController {
+
+  public static void main(String[] args) {
+    log.info("의찬 pull request");
+  }
+}
