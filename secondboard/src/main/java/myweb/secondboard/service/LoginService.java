418376846f53beadbf80be5f6d1bfff564adc2a2
euichanhwang <pine6971@naver.com>
2022-10-27 13:35:25 +0900

    로그인 예외 처리

--- secondboard/src/main/java/myweb/secondboard/service/LoginService.java
+++ secondboard/src/main/java/myweb/secondboard/service/LoginService.java
@@ -1,6 +1,7 @@
 package myweb.secondboard.service;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import myweb.secondboard.domain.Member;
 import myweb.secondboard.repository.MemberRepository;
@@ -19,7 +20,13 @@ public class LoginService {
   public Member login(String loginId, String password) throws NoSuchAlgorithmException {
     PasswordEncrypt passwordEncrypt = new PasswordEncrypt();
     String encryptedPassword = passwordEncrypt.encrypt(password);
-    return memberRepository.findByLoginId(loginId).filter(m -> m.getPassword()
-      .equals(encryptedPassword)).orElse(null);
+    List<Member> members = memberRepository.findAll();
+    for (Member member : members) {
+      if (member.getLoginId().equals(loginId)) {
+        return memberRepository.findByLoginId(loginId).filter(m -> m.getPassword()
+            .equals(encryptedPassword)).orElse(null);
+      }
+    }
+    return null;
   }
 }
