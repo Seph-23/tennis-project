2ec03fe68fd989a5ee3fcd40405247a688dd63e2
euichanhwang <pine6971@naver.com>
2022-10-28 00:39:41 +0900

    로그인 Id 저장

--- secondboard/src/main/java/myweb/secondboard/controller/HomeController.java
+++ secondboard/src/main/java/myweb/secondboard/controller/HomeController.java
@@ -1,9 +1,12 @@
 package myweb.secondboard.controller;
 
+import java.util.Locale;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Slf4j
 @Controller
@@ -13,10 +16,5 @@ public class HomeController {
   @GetMapping("/")
   public String home() {
     return "home";
-  }
-
-  @GetMapping("/abcd")
-  public String abcd() {
-    return "login/test";
   }
 }
