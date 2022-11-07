b959b997cc5fe9ed3c5db38d4052ee3d7cbfa3e2
Seph-23 <seongmlyu95@gmail.com>
2022-11-07 17:52:51 +0900

    .

--- secondboard/src/main/resources/static/js/kakaoMap.js
+++ secondboard/src/main/resources/static/js/kakaoMap.js
@@ -108,20 +108,7 @@ function displayPlaces(places) {
               lat: marker.getPosition().getLat(),
               lng: marker.getPosition().getLng()
             }
-            getAddr(addLocation.lat,addLocation.lng);
-            function getAddr(lat,lng){
-              let geocoder = new kakao.maps.services.Geocoder();
 
-              let coord = new kakao.maps.LatLng(lat, lng);
-              let callback = function(result, status) {
-                if (status === kakao.maps.services.Status.OK) {
-                  console.log(typeof result);
-                  console.log(JSON.stringify(result));
-                }
-              };
-
-              geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
-            }
 
           } else {
             return false;
