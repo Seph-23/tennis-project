ae8a46a4a97aa0f64b9a1e86ff6f4b835da11b3b
Seph-23 <seongmlyu95@gmail.com>
2022-11-15 14:31:26 +0900

    .

--- secondboard/src/main/java/myweb/secondboard/WebConfig.java
+++ secondboard/src/main/java/myweb/secondboard/WebConfig.java
@@ -14,7 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
         .order(1)
         .addPathPatterns("/**") //모든 경로에 대해 허용
         .excludePathPatterns("/", "/members/new", "/login", "/logout",
-            "/css/**", "/*.ico", "/error", "/js/**", "/images/**", "/matching/home", "/tournament", "/ranking/home", "/boards/home", "/club", "/boards/detail/**"
-        ,"/oauth/kakao/**", "/notice/home", "/notice/detail/**", "/question/home", "/question/detail/**", "/lesson/home", "/lesson/detail/**", "/login/modal/**", "/api/members/*");
+            "/css/**", "/*.ico", "/error", "/js/**", "/images/**", "/matching/home",
+          "/tournament", "/ranking/home", "/boards/home", "/club", "/boards/detail/**",
+          "/oauth/kakao/**", "/notice/home", "/notice/detail/**", "/question/home",
+          "/question/detail/**", "/lesson/home", "/lesson/detail/**", "/login/modal/**",
+          "/api/members/*", "/matching/matchingListUpdate/*");
   }
 }
