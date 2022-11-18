0e2b8eb22d791ae560e278f27fc87fd19b29b88b
Reese-Yoon <hyun5no@naver.com>
2022-11-18 19:08:22 +0900

    file, member 수정하고 profileHome수정

--- secondboard/src/main/java/myweb/secondboard/controller/MemberController.java
+++ secondboard/src/main/java/myweb/secondboard/controller/MemberController.java
@@ -68,14 +68,13 @@ public class MemberController {
   @GetMapping("/profile/{memberId}")
   public String profileHome(@PathVariable("memberId") Long memberId, Model model) {
 
-    Member member = memberService.findById(memberId);
+      Member member = memberService.findById(memberId);
+      model.addAttribute("member", member);
 
-    if(member.getFile() != null){
-      String src = new String(member.getFile().getSaveImg(), StandardCharsets.UTF_8);
-      model.addAttribute("src", src);
-    }
-
-    model.addAttribute("member", member);
+      if(member.getImgEn()!=null){
+          String src = new String(member.getImgEn(), StandardCharsets.UTF_8);
+          model.addAttribute("src", src);
+      }
 
     // 내정보 수정 모달용
     MemberUpdateForm form = new MemberUpdateForm();
