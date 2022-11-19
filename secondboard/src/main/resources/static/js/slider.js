6641dec0d5c4b4b6bd3e6655dde40e7ec62ed9c9
Juyeon703 <sh6244@naver.com>
2022-11-19 17:38:48 +0900

    matching date js, css

--- secondboard/src/main/resources/static/js/slider.js
+++ secondboard/src/main/resources/static/js/slider.js
@@ -13,3 +13,13 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+$(document).ready(function () {
+    $(".hidden_btn[value=" + '토' + "]").css("color", "blue")
+    $(".hidden_btn[value=" + '일' + "]").css("color", "red")
+    const clickDate = $("#m-date").attr("value")
+    $(".swiper-slide").removeClass("click-active")
+    $("#click-" + clickDate).addClass("click-active")
+    $("#click-" + clickDate).children("button").addClass("click-active")
+    $("#click-" + clickDate).children("button").css("color", "white")
+})
