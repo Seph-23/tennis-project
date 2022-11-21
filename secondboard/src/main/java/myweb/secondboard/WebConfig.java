fb06b343d72533e5805b263ec9d2deebfff40b38
euichanhwang <pine6971@naver.com>
2022-11-21 21:01:20 +0900

    에러 페이지 interceptor 수정

--- secondboard/src/main/java/myweb/secondboard/WebConfig.java
+++ secondboard/src/main/java/myweb/secondboard/WebConfig.java
@@ -10,16 +10,17 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
+
     registry.addInterceptor(new LoginCheckInterceptor())
         .order(1)
         .addPathPatterns("/**") //모든 경로에 대해 허용
-        .excludePathPatterns("/", "/members/new", "/login", "/logout",
-            "/css/**", "/*.ico", "/error", "/js/**", "/images/**", "/matching/home",
+        .excludePathPatterns("/", "/members/new", "/login", "/logout", "/errors/**",
+            "/css/**", "/*.ico", "/error/**", "/js/**", "/images/**", "/matching/home",
           "/tournament", "/ranking/home", "/boards/home", "/club", "/boards/detail/**",
           "/oauth/kakao/**", "/notice/home", "/notice/detail/**", "/question/home",
           "/question/detail/**", "/lesson/home", "/lesson/detail/**", "/login/modal/**",
           "/api/members/*", "/matching/matchingListUpdate/*", "/members/find/password/**",
             "/members/find/**", "/api/sms/check/sendSMS/**", "/members/update/**", "/members/profile/**", "/members/withdrawl/**",
-            "/error", "/matching/detail/**");
+            "/matching/detail/**"); // 인터셉터에서 제외할 패턴 : 로그인 안한 회원
   }
 }
