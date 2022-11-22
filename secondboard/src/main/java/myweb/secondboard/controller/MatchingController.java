5d5d990f002fe159a27fd3d47b0b8ae18304b8f8
Seph-23 <seongmlyu95@gmail.com>
2022-11-22 15:37:34 +0900

    matching controller fixed

--- secondboard/src/main/java/myweb/secondboard/controller/MatchingController.java
+++ secondboard/src/main/java/myweb/secondboard/controller/MatchingController.java
@@ -134,6 +134,12 @@ public class MatchingController {
     List<List<Player>> playerListA = new ArrayList<>();
     List<List<Player>> playerListB = new ArrayList<>();
 
+    for (int i = 0; i < playersA.size(); i++) {
+      playerListA.add(playerService.findByMemberId(playersA.get(i).getMember().getId()));
+    }
+    for (int i = 0; i < playersB.size(); i++) {
+      playerListB.add(playerService.findByMemberId(playersB.get(i).getMember().getId()));
+    }
 
     model.addAttribute("playerListA", playerListA);
     model.addAttribute("playerListB", playerListB);
