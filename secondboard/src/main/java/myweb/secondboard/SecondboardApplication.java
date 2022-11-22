3b74f137c2886605b01faedec09c9ed09246ec0f
Reese-Yoon <hyun5no@naver.com>
2022-11-22 19:41:29 +0900

    matching 프로필이미지 수정

--- secondboard/src/main/java/myweb/secondboard/SecondboardApplication.java
+++ secondboard/src/main/java/myweb/secondboard/SecondboardApplication.java
@@ -50,7 +50,7 @@ public class SecondboardApplication {
 
   @Bean(name = "uploadPath")
   public String uploadPath() {
-    return "/Users/seph/Documents/Dev/git/tennis-project/secondboard/src/main/resources/static/files";
+    return "C:\\Users\\master\\git\\GOGOTennis\\tennis-project\\secondboard\\src\\main\\resources\\static\\files";
   }
   @Bean
   JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
@@ -61,58 +61,58 @@ public class SecondboardApplication {
    * 주석 처리하지 말고
    * 회원 2개 생성해야 이미지 적용
    */
-  @Order(1)
-  @Bean
-  public CommandLineRunner initMemberProfile(MemberRepository memberRepository,
-    RecordRepository recordRepository, MemberImageService memberImageService, MemberService memberService) {
-    return args -> IntStream.rangeClosed(1, 2).forEach(i -> {
-      try {
-        //생성 객체 준비
-        Member member = new Member();
-        PasswordEncrypt passwordEncrypt = new PasswordEncrypt();
-        Record record = Record.createRecord();
-
-        member.setLoginId("testtest" + i);
-        member.setPassword(passwordEncrypt.encrypt("testtest" + i));
-        member.setNickname("test" + i);
-        member.setEmail("test" + i + "@test.com");
-        member.setBirthday("19950307");
-        member.setPhoneNumber("01021219" + String.format("%03d", i));
-        member.setProvider(Provider.GOGOTENNIS);
-        member.setRole(Role.MEMBER);
-        member.setTier(Tier.IRON);
-        member.setRecord(record);
-
-        if (i % 2 == 1) {
-          member.setGender(Gender.MALE);
-        } else {
-          member.setGender(Gender.FEMALE);
-        }
-
-        recordRepository.save(record);
-        memberRepository.save(member);
-
-        File fileItem = null;
-        try {
-          if (member.getGender().toString().equals(Gender.MALE.toString())) {
-            fileItem = new File(
-              "/Users/seph/Documents/Dev/작업중/tennis-project/secondboard/src/main/resources/static/images/profile_ma.png");
-          } else {
-            fileItem = new File(
-              "/Users/seph/Documents/Dev/작업중/tennis-project/secondboard/src/main/resources/static/images/profile_fe.png");
-          }
-          FileInputStream input = new FileInputStream(fileItem);
-          MultipartFile multipartFile = new MockMultipartFile("fileItem",
-            fileItem.getName(), "image/png", IOUtils.toByteArray(input));
-          MemberUploadFile uploadFile = memberImageService.newStore(multipartFile);
-          memberService.setBasicProfileImage("/image/member/"+uploadFile.getId(), member.getId());
-        } catch (Exception e) {
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    });
-  }
+//  @Order(1)
+//  @Bean
+//  public CommandLineRunner initMemberProfile(MemberRepository memberRepository,
+//    RecordRepository recordRepository, MemberImageService memberImageService, MemberService memberService) {
+//    return args -> IntStream.rangeClosed(1, 2).forEach(i -> {
+//      try {
+//        //생성 객체 준비
+//        Member member = new Member();
+//        PasswordEncrypt passwordEncrypt = new PasswordEncrypt();
+//        Record record = Record.createRecord();
+//
+//        member.setLoginId("testtest" + i);
+//        member.setPassword(passwordEncrypt.encrypt("testtest" + i));
+//        member.setNickname("test" + i);
+//        member.setEmail("test" + i + "@test.com");
+//        member.setBirthday("19950307");
+//        member.setPhoneNumber("01021219" + String.format("%03d", i));
+//        member.setProvider(Provider.GOGOTENNIS);
+//        member.setRole(Role.MEMBER);
+//        member.setTier(Tier.IRON);
+//        member.setRecord(record);
+//
+//        if (i % 2 == 1) {
+//          member.setGender(Gender.MALE);
+//        } else {
+//          member.setGender(Gender.FEMALE);
+//        }
+//
+//        recordRepository.save(record);
+//        memberRepository.save(member);
+//
+//        File fileItem = null;
+//        try {
+//          if (member.getGender().toString().equals(Gender.MALE.toString())) {
+//            fileItem = new File(
+//              "C:\\Users\\master\\git\\GOGOTennis\\tennis-project\\secondboard\\src\\main\\resources\\static\\files\\profile_ma.png");
+//          } else {
+//            fileItem = new File(
+//              "C:\\Users\\master\\git\\GOGOTennis\\tennis-project\\secondboard\\src\\main\\resources\\static\\files\\profile_fe.png");
+//          }
+//          FileInputStream input = new FileInputStream(fileItem);
+//          MultipartFile multipartFile = new MockMultipartFile("fileItem",
+//            fileItem.getName(), "image/png", IOUtils.toByteArray(input));
+//          MemberUploadFile uploadFile = memberImageService.newStore(multipartFile);
+//          memberService.setBasicProfileImage("/image/member/"+uploadFile.getId(), member.getId());
+//        } catch (Exception e) {
+//        }
+//      } catch (Exception e) {
+//        e.printStackTrace();
+//      }
+//    });
+//  }
 
   /**
    * 멤버 정보
@@ -191,42 +191,42 @@ public class SecondboardApplication {
 //      }
 //    });
 //  }
-//
-//  public static int rand(int min, int max)
-//	{
-//		if (min > max || (max - min + 1 > Integer.MAX_VALUE)) {
-//			throw new IllegalArgumentException("Invalid range");
-//		}
-//
-//		return new Random().nextInt(max - min + 1) + min;
-//	}
-//
-//	public static int generate() {
-//		int r;
-//		do {
-//			r = 5 * (rand(1, 5) - 1) + rand(1, 5);
-//		} while (r > 7);
-//
-//		return r;
-//	}
-//
-//	@Order(3)
-//	@Bean
-//	public CommandLineRunner test(LocalRepository localRepository, TournamentRepository tournamentRepository) {
-//        return args -> {
-//            //==Local (지역) 테스트 데이터==//
-//            List<String> locals = new ArrayList<>();
-//            for (String s : Arrays.asList("서울", "경기", "강원", "경상", "전라", "충청", "제주")) {
-//                locals.add(s);
-//            }
-//
-//            for (int i = 1; i <= 7; i++) {
-//                Local local = new Local();
-//                local.setName(locals.get(i - 1));
-//                localRepository.save(local);
-//            }
-//        };
-//    }
+
+  public static int rand(int min, int max)
+	{
+		if (min > max || (max - min + 1 > Integer.MAX_VALUE)) {
+			throw new IllegalArgumentException("Invalid range");
+		}
+
+		return new Random().nextInt(max - min + 1) + min;
+	}
+
+	public static int generate() {
+		int r;
+		do {
+			r = 5 * (rand(1, 5) - 1) + rand(1, 5);
+		} while (r > 7);
+
+		return r;
+	}
+
+	@Order(3)
+	@Bean
+	public CommandLineRunner test(LocalRepository localRepository, TournamentRepository tournamentRepository) {
+        return args -> {
+            //==Local (지역) 테스트 데이터==//
+            List<String> locals = new ArrayList<>();
+            for (String s : Arrays.asList("서울", "경기", "강원", "경상", "전라", "충청", "제주")) {
+                locals.add(s);
+            }
+
+            for (int i = 1; i <= 7; i++) {
+                Local local = new Local();
+                local.setName(locals.get(i - 1));
+                localRepository.save(local);
+            }
+        };
+    }
 //
 ////  == 관리자 데이터 11.16(수) 테스트 완료 ==//
 //  @Order(4)
