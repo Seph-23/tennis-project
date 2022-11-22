50efa25f0523d03bc3996b6100b2792df79eee96
rkdhs321 <rkdhs321@gmail.com>
2022-11-23 00:20:59 +0900

    게시판 상세 수정

--- secondboard/src/main/java/myweb/secondboard/SecondboardApplication.java
+++ secondboard/src/main/java/myweb/secondboard/SecondboardApplication.java
@@ -50,7 +50,7 @@ public class SecondboardApplication {
 
   @Bean(name = "uploadPath")
   public String uploadPath() {
-    return "/Users/anchiun/project/tennis-project/secondboard/src/main/resources/static/files";
+    return "/Users/gaon/git/tennis-project/secondboard/src/main/resources/static/files";
   }
   @Bean
   JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
@@ -96,10 +96,10 @@ public class SecondboardApplication {
         try {
           if (member.getGender().toString().equals(Gender.MALE.toString())) {
             fileItem = new File(
-              "/Users/anchiun/project/tennis-project/secondboard/src/main/resources/static/files/profile_ma.png");
+              "/Users/gaon/git/tennis-project/secondboard/src/main/resources/static/files/profile_ma.png");
           } else {
             fileItem = new File(
-              "/Users/anchiun/project/tennis-project/secondboard/src/main/resources/static/files/profile_fe.png");
+              "/Users/gaon/git/tennis-project/secondboard/src/main/resources/static/files/profile_fe.png");
           }
           FileInputStream input = new FileInputStream(fileItem);
           MultipartFile multipartFile = new MockMultipartFile("fileItem",
