0113d1be371dd13af94cf5baaa03e88e5f0ec5e5
Seph-23 <seongmlyu95@gmail.com>
2022-11-22 11:34:20 +0900

    .

--- secondboard/src/main/java/myweb/secondboard/interceptor/LoginCheckInterceptor.java
+++ secondboard/src/main/java/myweb/secondboard/interceptor/LoginCheckInterceptor.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class LoginCheckInterceptor implements HandlerInterceptor {
 
   //로그인 한 유저 허용 범위
-  private static final String[] whitelist = {"boards/boardAdd", "/boards/new", "/lesson/lessonAdd",
+  private static final String[] whitelist = {"/boards/boardAdd", "/boards/new", "/lesson/lessonAdd",
     "/lesson/new", "/boardDelete/**", "/api/notice/noticeDelete/**", "/notice/update/**", "/notice/like",
     "/question/questionAdd", "/question/new", "/notice/new", "/club/memberDelete",
     "/club/delete", "/club/memberBan/*", "/club/join", "/members/update/password/*", "/members/withdrawl/**",
