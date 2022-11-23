d2345aef44c672c89a5f1df05b4bfce3d7742ff4
Seph-23 <seongmlyu95@gmail.com>
2022-11-23 14:24:36 +0900

    ranking page js changed

--- secondboard/src/main/resources/static/js/tierCheck.js
+++ secondboard/src/main/resources/static/js/tierCheck.js
@@ -45,71 +45,74 @@ const tiercheckModal = (checkmodal) => {
     }
 }
 
-const topRank = $("#topRank").text()
+$(document).ready(function () {
+    const topRank = $("#topRank").text()
 
-if( topRank == "IRON") {
-    $(".profile").attr("src", "/images/iron.png")
-} else if (topRank == "BRONZE") {
-    $(".profile").attr("src", "/images/bronze.png")
-} else if (topRank == "SILVER") {
-    $(".profile").attr("src", "/images/silver.png")
-} else if (topRank == "GOLD") {
-    $(".profile").attr("src", "/images/gold.png")
-} else if (topRank == "PLATINUM") {
-    $(".profile").attr("src", "/images/platinum.png")
-} else if (topRank == "DIAMOND") {
-    $(".profile").attr("src", "/images/diamond.png")
-} else if (topRank == "MASTER") {
-    $(".profile").attr("src", "/images/master.png")
-} else if (topRank == "GRANDMASTER") {
-    $(".profile").attr("src", "/images/grandmaster.png")
-} else if (topRank == "CHALLENGER") {
-    $(".profile").attr("src", "/images/challenger.png")
-}
+    if( topRank == "IRON") {
+        $(".profile_first").attr("src", "/images/iron.png")
+    } else if (topRank == "BRONZE") {
+        $(".profile_first").attr("src", "/images/bronze.png")
+    } else if (topRank == "SILVER") {
+        $(".profile_first").attr("src", "/images/silver.png")
+    } else if (topRank == "GOLD") {
+        $(".profile_first").attr("src", "/images/gold.png")
+    } else if (topRank == "PLATINUM") {
+        $(".profile_first").attr("src", "/images/platinum.png")
+    } else if (topRank == "DIAMOND") {
+        $(".profile_first").attr("src", "/images/diamond.png")
+    } else if (topRank == "MASTER") {
+        $(".profile_first").attr("src", "/images/master.png")
+    } else if (topRank == "GRANDMASTER") {
+        $(".profile_first").attr("src", "/images/grandmaster.png")
+    } else if (topRank == "CHALLENGER") {
+        $(".profile_first").attr("src", "/images/challenger.png")
+    }
 
-const secondRank = $("#secondRank").text()
+    const secondRank = $("#secondRank").text()
 
-if( secondRank == "IRON") {
-    $(".profile").attr("src", "/images/iron.png")
-} else if (secondRank == "BRONZE") {
-    $(".profile").attr("src", "/images/bronze.png")
-} else if (secondRank == "SILVER") {
-    $(".profile").attr("src", "/images/silver.png")
-} else if (secondRank == "GOLD") {
-    $(".profile").attr("src", "/images/gold.png")
-} else if (secondRank == "PLATINUM") {
-    $(".profile").attr("src", "/images/platinum.png")
-} else if (secondRank == "DIAMOND") {
-    $(".profile").attr("src", "/images/diamond.png")
-} else if (secondRank == "MASTER") {
-    $(".profile").attr("src", "/images/master.png")
-} else if (secondRank == "GRANDMASTER") {
-    $(".profile").attr("src", "/images/grandmaster.png")
-} else if (secondRank == "CHALLENGER") {
-    $(".profile").attr("src", "/images/challenger.png")
-}
+    if( secondRank == "IRON") {
+        $(".profile_second").attr("src", "/images/iron.png")
+    } else if (secondRank == "BRONZE") {
+        $(".profile_second").attr("src", "/images/bronze.png")
+    } else if (secondRank == "SILVER") {
+        $(".profile_second").attr("src", "/images/silver.png")
+    } else if (secondRank == "GOLD") {
+        $(".profile_second").attr("src", "/images/gold.png")
+    } else if (secondRank == "PLATINUM") {
+        $(".profile_second").attr("src", "/images/platinum.png")
+    } else if (secondRank == "DIAMOND") {
+        $(".profile_second").attr("src", "/images/diamond.png")
+    } else if (secondRank == "MASTER") {
+        $(".profile_second").attr("src", "/images/master.png")
+    } else if (secondRank == "GRANDMASTER") {
+        $(".profile_second").attr("src", "/images/grandmaster.png")
+    } else if (secondRank == "CHALLENGER") {
+        $(".profile_second").attr("src", "/images/challenger.png")
+    }
 
-let thirdRank = $("#thirdRank").text()
+    let thirdRank = $("#thirdRank").text()
 
-if( thirdRank == "IRON") {
-    $(".profile").attr("src", "/images/iron.png")
-} else if (thirdRank == "BRONZE") {
-    $(".profile").attr("src", "/images/bronze.png")
-} else if (thirdRank == "SILVER") {
-    $(".profile").attr("src", "/images/silver.png")
-} else if (thirdRank == "GOLD") {
-    $(".profile").attr("src", "/images/gold.png")
-} else if (thirdRank == "PLATINUM") {
-    $(".profile").attr("src", "/images/platinum.png")
-} else if (thirdRank == "DIAMOND") {
-    $(".profile").attr("src", "/images/diamond.png")
-} else if (thirdRank == "MASTER") {
-    $(".profile").attr("src", "/images/master.png")
-} else if (thirdRank == "GRANDMASTER") {
-    $(".profile").attr("src", "/images/grandmaster.png")
-} else if (thirdRank == "CHALLENGER") {
-    $(".profile").attr("src", "/images/challenger.png")
-}
+    if( thirdRank == "IRON") {
+        $(".profile_third").attr("src", "/images/iron.png")
+    } else if (thirdRank == "BRONZE") {
+        $(".profile_third").attr("src", "/images/bronze.png")
+    } else if (thirdRank == "SILVER") {
+        $(".profile_third").attr("src", "/images/silver.png")
+    } else if (thirdRank == "GOLD") {
+        $(".profile_third").attr("src", "/images/gold.png")
+    } else if (thirdRank == "PLATINUM") {
+        $(".profile_third").attr("src", "/images/platinum.png")
+    } else if (thirdRank == "DIAMOND") {
+        $(".profile_third").attr("src", "/images/diamond.png")
+    } else if (thirdRank == "MASTER") {
+        $(".profile_third").attr("src", "/images/master.png")
+    } else if (thirdRank == "GRANDMASTER") {
+        $(".profile_third").attr("src", "/images/grandmaster.png")
+    } else if (thirdRank == "CHALLENGER") {
+        $(".profile_third").attr("src", "/images/challenger.png")
+    }
+});
+
 
 //
 // function tiercheckModal() {
